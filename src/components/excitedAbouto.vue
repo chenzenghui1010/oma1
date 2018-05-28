3607237f477ec1eca423d6e1d994ecc8d12172ed
http://192.168.1.100:8888/h5/sdk.git <>
2018-05-28 18:34:33 +0800

    保存28

--- src/components/excitedAbouto.vue
+++ src/components/excitedAbouto.vue
@@ -1,30 +1,34 @@
 <template>
   <div class="excitedo">
-      <mtitle/>
+    <mtitle/>
     <div class="company">
       <p class="title1"><span>{{title1}}</span></p>
-      <x-input title=" <span>*</span> 姓名：" required="required" v-model="eName" placeholder="请输入" is-type="china-name">
+      <x-input title=" <span>*</span> 姓名：" required="required" v-model="eName" placeholder="请输入"
+               is-type="china-name">
         <span>*</span></x-input>
-      <x-input title=" <span>*</span> 手机号：" required="required" mask="999 9999 9999" v-model="ePoints" placeholder="请输入"
+      <x-input title=" <span>*</span> 手机号：" required="required" mask="999 9999 9999" v-model="ePoints"
+               placeholder="请输入"
                :max="13"
                is-type="china-mobile"></x-input>
 
       <popup-picker title=" <span>*</span> 证件号：" :data="list1" v-model="eLicense" maxlenth="18"></popup-picker>
-      <x-input  id="none" title=" " required="required" placeholder="请输入" v-model="eLicenseNumber"> is-type="china-name"></x-input>
+      <x-input id="none" title=" " required="required" placeholder="请输入" v-model="eLicenseNumber">
+        is-type="china-name">
+      </x-input>
 
       <popup-picker title=" &nbsp 车牌号：" :data="Car" v-model="eCar" maxlength="7"></popup-picker>
       <x-input title=" " v-model="eCarNumber" placeholder="请输入"></x-input>
 
 
       <x-input title=" <span>*</span> 公司：" required="required" v-model="eCompany" placeholder="请输入"
-               ></x-input>
+      ></x-input>
 
-      <span>*</span>
+      <span>* </span>
       <datetime v-model="eStart" format="YYYY-MM-DD HH:mm" :min-hour=9 :max-hour=18 inline-desc=' 来访时间：'
                 placeholder="2018-05-10 10:00"></datetime>
 
-      <span>*</span>
-      <datetime v-model="eEnd" format="YYYY-MM-DD HH:mm" :min-hour=9 :max-hour=18 inline-desc='预计离开：'
+      <span>* </span>
+      <datetime v-model="eEnd" format="YYYY-MM-DD HH:mm" :min-hour=9 :max-hour=18 inline-desc=' 预计离开：'
                 placeholder="2018-05-10 10:00"></datetime>
       <x-input title=" <span>*</span> 来访事由：" required="required" v-model="eCause" placeholder="请输入"></x-input>
 
@@ -51,11 +55,10 @@
         <div class="addk">
         </div>
       </div>
-
-
     </div>
+
     <div class="add">
-      <ul  @click="add">
+      <ul @click="add">
         <li><img class="addimg" src="../assets/添加@2x.png" alt=""></li>
         <li>添加随行人员</li>
       </ul>
@@ -90,7 +93,7 @@
     components: {
       Confirm, TransferDomDirective, TransferDom,
       Datetime, PopupPicker, Picker, Divider, XSwitch,
-      mtitle,AlertModule,
+      mtitle, AlertModule,
       XInput,
       XButton,
       Group,
@@ -99,105 +102,125 @@
     },
     data() {
       return {
-        follower:[{fName:'',fLicense:['身份证'],fLicenseNumber:'', title:[['身份证']],shows:false}],
+        follower: [{
+          fName: '1111',
+          fLicense: ['身份证'],
+          fLicenseNumber: this.$store.state.follower.fLicenseNumber,
+          title: [['身份证']],
+          shows: false
+        }],
+        // fName: this.$store.state.fName,
+        // fLicense: [this.$store.state.fLicense],
+        // fLicenseNumber: this.$store.state.fLicenseNumber,
 
         list1: [['身份证']],
         Car: [['请选择', '京', '津', '沪', '渝', '冀', '豫', '云', '辽', '黑', '湘', '皖', '鲁', '新', '苏', '浙', '赣', '鄂', '桂', '甘', '晋', '蒙', '陕', '吉', '闽', '贵', '粤', '青', '藏', '川', '宁', '琼']],
         title1: '来访人信息',
         title2: '随行人员信息',
+
         eName: this.$store.state.eName,
         ePoints: this.$store.state.ePoints,
         eLicense: [this.$store.state.eLicense],
-        eLicense2:[],
+        eLicense2: [],
         eLicenseNumber: this.$store.state.eLicenseNumber,
-
         eCar: [this.$store.state.eCar],
         eCarNumber: this.$store.state.eCarNumber,
         eCompany: this.$store.state.eCompany,
         eStart: this.$store.state.eStart,
         eEnd: this.$store.state.eEnd,
         eCause: this.$store.state.eCause,
-
-        eList:[],
+        eList: [],
 
         showExcitedO: true,
       }
     },
     computed: {},
-    created(){
+    created() {
 
     },
     methods: {
 
       add: function () {
 
-        this.follower.push({fName:'',fLicense:['身份证'],fLicenseNumber:'', title:[['身份证']],shows:false})
+        this.follower.push({
+          'fName':this.$store.state.fName,
+          'fLicense': ['身份证'],
+          'fLicenseNumber': this.$store.state.fLicenseNumber,
+          'title': [['身份证']],
+          'shows': false
+        })
       },
 
       //上一步
-      last:function(){
+      last: function () {
 
         history.go(-1);
       },
       //预 览
       excited: function () {
+        let _this = this.$store.dispatch
 
-        // for(let i=0;i<this.follower.length;i++){
-        //   alert(this.follower[i].fName)
+        _this('eName', this.eName),
+        _this('ePoints', this.ePoints),
+        _this('eLicense', this.eLicense.toString()),
+        _this('eLicenseNumber', this.eLicenseNumber),
+        _this('eCar', this.eCar.toString()),
+        _this('eCarNumber', this.eCarNumber),
+        _this('eCompany', this.eCompany),
+        _this('eStart', this.eStart),
+        _this('eEnd', this.eEnd),
+        _this('eCause', this.eCause);
+        //随行人
+
+
+        //_this('follower', [{'fName':this.follower.fName ,'fLicense':[this.follower.fLicense],'fLicenseNumber':this.follower.fLicenseNumber}]);
+        // alert(this.$store.dispatch('follower',this.follower.fName))
+        // for (let i = 0; i < this.$store.state.follower.length; i++) {
         //
+        //   alert(this.$store.state.follower[i].fName)
         // }
-        this.$store.commit('eName', this.eName);
-        this.$store.commit('ePoints', this.ePoints);
-        this.$store.commit('eLicense', this.eLicense.toString());
-        this.$store.commit('eLicenseNumber', this.eLicenseNumber);
-        this.$store.commit('eCar', this.eCar.toString());
-        this.$store.commit('eCarNumber', this.eCarNumber);
-        this.$store.commit('eCompany', this.eCompany);
-        this.$store.commit('eStart', this.eStart);
-        this.$store.commit('eEnd', this.eEnd);
-        this.$store.commit('eCause', this.eCause);
-        this.$store.commit('follower',);
+
 
         // this.$router.push({path: 'excitedaboutok'})
-        if(this.eName == '' || this.ePoints == '' || this.eLicense == '' || this.eLicenseNumber == '' ||
-
-          this.eCompany == '' || this.eStart == '' ||this.eEnd == '' || this.eCause == ''){
-
-          AlertModule.show({title:'请填写完整信息'})
-
-          return false
-        }
-        let url=this.HOST + '/mv/visit/reserve'
-       this.$axios.post(url,{
-         name:this.eName,
-         phone:this.ePoints,
-         identityNo:this.eLicenseNumber,
-         identityType:this.eLicense,
-         company:this.eCompany,
-         scheduledInTime:this.eStart,
-         scheduledOutTime:this.eEnd,
-         subject:this.eCause,
-         follower:{'name':'你好','identityNo':'431102198912258414','identityType':'身份证'}
+        // if(this.eName == '' || this.ePoints == '' || this.eLicense == '' || this.eLicenseNumber == '' ||
+        //
+        //   this.eCompany == '' || this.eStart == '' ||this.eEnd == '' || this.eCause == ''){
+        //
+        //   AlertModule.show({title:'请填写完整信息'})
+        //
+        //   return false
+        // }
+        let url = this.HOST + '/mv/visit/reserve'
+        this.$axios.post(url, {
+          name: this.eName,
+          phone: this.ePoints,
+          identityNo: this.eLicenseNumber,
+          identityType: this.eLicense,
+          company: this.eCompany,
+          scheduledInTime: this.eStart,
+          scheduledOutTime: this.eEnd,
+          subject: this.eCause,
+          follower: {'name': '你好', 'identityNo': '431102198912258414', 'identityType': '身份证'}
         })
-         .then(res =>{
+          .then(res => {
             this.$router.push({path: 'excitedaboutok'})
-         })
-         .catch(error =>{
-           console.log(error)
-        })
+          })
+          .catch(error => {
+            console.log(error)
+          })
 
 
         // this.$router.push({path: 'excitedaboutok'})
       },
       delO: function (index) {
-          this.follower[index].shows=true;
+        this.follower[index].shows = true;
       },
       //取消
       onCancel: function () {
       },
       //确定
       onConfirm: function () {
-          this.follower.splice(this.follower.length-1, 1)
+        this.follower.splice(this.follower.length - 1, 1)
       },
     }
   }
@@ -207,8 +230,6 @@
     padding: 0;
     margin: 0;
   }
-
-
 
   span {
     margin: 8px 10px 0 15px;
@@ -225,12 +246,14 @@
     height: 20px;
     padding: 10px 15px;
   }
-.addk{
-  width: 100%;
-  height: 10px;
-  background: #edf1f3;
 
-}
+  .addk {
+    width: 100%;
+    height: 10px;
+    background: #edf1f3;
+
+  }
+
   .excitedo {
 
     height: 100px;
@@ -244,7 +267,7 @@
           margin: 12px 0 0 4%;
           color: #999;
         }
-        .adds{
+        .adds {
         }
       }
     }
@@ -312,7 +335,6 @@
 
     }
 
-
     .add {
       padding-bottom: 30px;
       width: 100%;
@@ -351,7 +373,7 @@
           outline: none;
         }
       }
-      button:nth-child(1){
+      button:nth-child(1) {
         background: #fff;
         color: #1d83c5;
 
--- src/store.js
+++ src/store.js
@@ -1,124 +1,197 @@
 import Vue from 'vue'
-import  vuex from 'vuex'
+import vuex from 'vuex'
+
 Vue.use(vuex);
 
 export default new vuex.Store({
-  state:{
+  state: {
     // 受访人信息
-    mName:'',
-    mPoints:'',
-    mCompany:'',
-    mDepartment:'',
+    mName: '',
+    mPoints: '',
+    mCompany: '',
+    mDepartment: '',
 
     // 来访人信息
-    eName:'',
-    ePoints:'',
-    eLicense:'身份证',
-    eLicenseNumber:'',
-    eCar:'请选择',
-    eCarNumber:'',
-    eCompany:'',
+
+    eName: '',
+    ePoints: '',
+    eLicense: '身份证',
+    eLicenseNumber: '',
+    eCar: '请选择',
+    eCarNumber: '',
+    eCompany: '',
     eStart: '',
     eEnd: '',
-    eCause:'',
+    eCause: '',
+
+
+    // eName: '',
+    // ePoints: '',
+    // eLicense: '身份证',
+    // eLicenseNumber: '',
+    // eCar: '请选择',
+    // eCarNumber: '',
+    // eCompany: '',
+    // eStart: '',
+    // eEnd: '',
+    // eCause: '',
 
     //来访邀约
 
-    iName:'',
-    iPoints:'',
-    iLicense:'',
-    iLicenseNumber:'',
-    iCar:'',
-    iCarNumber:'',
-    iCompany:'',
+    iName: '',
+    iPoints: '',
+    iLicense: '',
+    iLicenseNumber: '',
+    iCar: '',
+    iCarNumber: '',
+    iCompany: '',
     iStart: '',
     iEnd: '',
-    iCause:'',
+    iCause: '',
 
     //随行人
 
-    follower:[{fName:'',fLicense:'',fLicenseNumber:'',title:'身份证'},],
-
-
-
+    // follower: [{'fName': '', 'fLicense': ['身份证'], 'fLicenseNumber': '', 'title': '身份证'}],
+    follower: [],
+    fName: '',
+    fLicense: ['身份证'],
+    fLicenseNumber: '',
   },
-  mutations:{
-    mName(state ,name){
-      state.mName=name;
+  mutations: {
+    mName: (state, name) => {
+      state.mName = name;
     },
-    mPoints(state ,points){
-      state.mPoints=points;
+    mPoints(state, points) {
+      state.mPoints = points;
     },
-    mCompany(state ,company){
-      state.mCompany=company;
+    mCompany(state, company) {
+      state.mCompany = company;
     },
-    mDepartment(state ,department){
-      state.mDepartment=department;
+    mDepartment(state, department) {
+      state.mDepartment = department;
     },
 
     // 来访人信息
-    eName( state, name ){
-      state.eName=name;
+
+    // exc: (state, sxc) => {
+    //   state.exc = sxc;
+    // },
+
+    eName(state, name) {
+      state.eName = name;
     },
-    ePoints( state, points ){
-      state.ePoints=points;
+    ePoints(state, points) {
+      state.ePoints = points;
     },
-    eLicense( state, license ){
-      state.eLicense=license;
+    eLicense(state, license) {
+      state.eLicense = license;
     },
-    eLicenseNumber( state, eLicenseNumber ){
-      state.eLicenseNumber=eLicenseNumber;
+    eLicenseNumber(state, eLicenseNumber) {
+      state.eLicenseNumber = eLicenseNumber;
     },
-    eCar( state, eCar ){
-      state.eCar=eCar;
+    eCar(state, eCar) {
+      state.eCar = eCar;
     },
-    eCarNumber( state, eCarNumber ){
-      state.eCarNumber=eCarNumber;
+    eCarNumber(state, eCarNumber) {
+      state.eCarNumber = eCarNumber;
     },
-    eCompany( state, eCompany ){
-      state.eCompany=eCompany;
+    eCompany(state, eCompany) {
+      state.eCompany = eCompany;
     },
-    eStart( state, eStart ){
-      state.eStart=eStart;
+    eStart(state, eStart) {
+      state.eStart = eStart;
     },
-    eEnd( state, eEnd ){
-      state.eEnd=eEnd;
+    eEnd(state, eEnd) {
+      state.eEnd = eEnd;
     },
-    eCause( state, eCause ){
-      state.eCause=eCause;
+    eCause(state, eCause) {
+      state.eCause = eCause;
     },
 
-     //来访邀约
-    iName( state, iName ){
-      state.iName=iName;
+    //来访邀约
+    iName(state, iName) {
+      state.iName = iName;
     },
-    iPoints( state, iPoints ){
-      state.iPoints=iPoints;
+    iPoints(state, iPoints) {
+      state.iPoints = iPoints;
     },
-    iLicense( state, iLicense ){
-      state.iLicense=iLicense;
+    iLicense(state, iLicense) {
+      state.iLicense = iLicense;
     },
-    iLicenseNumber( state, iLicenseNumber ){
-      state.iLicenseNumber=iLicenseNumber;
+    iLicenseNumber(state, iLicenseNumber) {
+      state.iLicenseNumber = iLicenseNumber;
     },
-    iCar( state, iCar ){
-      state.iCar=iCar;
+    iCar(state, iCar) {
+      state.iCar = iCar;
     },
-    iCarNumber( state, iCarNumber ){
-      state.iCarNumber=iCarNumber;
+    iCarNumber(state, iCarNumber) {
+      state.iCarNumber = iCarNumber;
     },
-    iCompany( state, iCompany ){
-      state.iCompany=iCompany;
+    iCompany(state, iCompany) {
+      state.iCompany = iCompany;
     },
-    iStart( state, iStart ){
-      state.iStart=iStart;
+    iStart(state, iStart) {
+      state.iStart = iStart;
     },
-    iEnd( state, iEnd ){
-      state.iEnd=iEnd;
+    iEnd(state, iEnd) {
+      state.iEnd = iEnd;
     },
-    iCause( state, iCause ){
-      state.iCause=iCause;
+    iCause(state, iCause) {
+      state.iCause = iCause;
     },
 
-  }
+
+    //随行人
+    follower(state, follower) {
+      state.follower = follower
+    },
+
+  },
+
+  actions: {
+
+    follower({commit}, foll) {
+      return new Promise(resolve => {
+        commit('follower', foll)
+        resolve();
+      })
+    },
+
+
+    // 来访人信息
+    eName({commit}, eName) {
+      // return new Promise(resolve => {
+      commit('eName', eName)
+      // resolve();
+      // })
+    },
+    ePoints({commit}, ePoints) {
+      commit('ePoints', ePoints)
+    },
+    eLicense({commit}, eLicense) {
+      commit('eLicense', eLicense)
+    },
+    eLicenseNumber({commit}, eLicenseNumber) {
+      commit('eLicenseNumber', eLicenseNumber)
+    },
+    eCar({commit}, eCar) {
+      commit('eCar', eCar)
+    },
+    eCarNumber({commit}, eCarNumber) {
+      commit('eCarNumber', eCarNumber)
+    },
+    eCompany({commit}, eCompany) {
+      commit('eCompany', eCompany)
+    },
+    eStart({commit}, eStart) {
+      commit('eStart', eStart)
+    },
+    eEnd({commit}, eEnd) {
+      commit('eEnd', eEnd)
+    },
+    eCause({commit}, eCause) {
+      commit('eCause', eCause)
+    },
+
+  },
 })
